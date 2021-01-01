c01168cef5e8ccca44d720a1112cf9d4253704ac
Jujare Vinayak <38416326+JujareVinayak@users.noreply.github.com>
2021-01-01 13:37:48 +0530

    Added business logic for Viewmodel.

--- app/src/main/java/com/vinayak/justcleanassessment/viewmodel/MainViewModel.kt
+++ app/src/main/java/com/vinayak/justcleanassessment/viewmodel/MainViewModel.kt
@@ -1,4 +1,46 @@
 package com.vinayak.justcleanassessment.viewmodel
 
-class MainViewModel {
+import androidx.hilt.lifecycle.ViewModelInject
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import androidx.navigation.fragment.NavHostFragment.findNavController
+import androidx.navigation.fragment.findNavController
+import com.vinayak.justcleanassessment.data.Post
+import com.vinayak.justcleanassessment.data.api.MainRepository
+import com.vinayak.justcleanassessment.utils.Resource
+import kotlinx.coroutines.launch
+import java.net.UnknownHostException
+
+class MainViewModel @ViewModelInject constructor(
+    private val mainRepository: MainRepository
+): ViewModel(){
+
+    private val _res = MutableLiveData<Resource<List<Post>>>()
+
+    val res : LiveData<Resource<List<Post>>>
+        get() = _res
+
+    init {
+        getPosts()
+    }
+
+    private fun getPosts()  = viewModelScope.launch {
+        _res.postValue(Resource.loading(null))
+        try {
+            mainRepository.getPosts().let {
+                if (it.isSuccessful) {
+                    _res.postValue(Resource.success(it.body()))
+                } else {
+                    _res.postValue(Resource.error(it.errorBody().toString(), null))
+                }
+            }
+        }catch (e: UnknownHostException){
+            _res.postValue(Resource.error("No Internet Connection",null))
+        }
+    }
+
+
+
 }
