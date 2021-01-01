3b1edeb275fafe1334f0745c91e16d8ab306f92f
Jujare Vinayak <38416326+JujareVinayak@users.noreply.github.com>
2021-01-01 13:37:14 +0530

    Navigation components setup.

--- app/src/main/java/com/vinayak/justcleanassessment/MainActivity.kt
+++ app/src/main/java/com/vinayak/justcleanassessment/MainActivity.kt
@@ -1,11 +1,29 @@
 package com.vinayak.justcleanassessment
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.viewModels
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.setupActionBarWithNavController
+import com.vinayak.justcleanassessment.viewmodel.MainViewModel
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+    private lateinit var navController: NavController
+
+    private val mainViewModel: MainViewModel by viewModels()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        navController = navHostFragment.findNavController()
+
+        setupActionBarWithNavController(navController)
+
     }
-}
+    }
