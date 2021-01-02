3f855200904b11f9a1e8927bd9fe71328e9dbc71
Jujare Vinayak <38416326+JujareVinayak@users.noreply.github.com>
2021-01-02 13:26:05 +0530

    Added Progress dialog.

--- app/src/main/java/com/vinayak/justcleanassessment/ui/PostFragment.kt
+++ app/src/main/java/com/vinayak/justcleanassessment/ui/PostFragment.kt
@@ -1,7 +1,16 @@
 package com.vinayak.justcleanassessment.ui
 
+import android.app.AlertDialog
+import android.content.Context
+import android.graphics.Color
 import android.os.Bundle
+import android.view.Gravity
 import android.view.View
+import android.view.ViewGroup
+import android.view.WindowManager
+import android.widget.LinearLayout
+import android.widget.ProgressBar
+import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.recyclerview.widget.LinearLayoutManager
@@ -19,10 +28,12 @@ import dagger.hilt.android.AndroidEntryPoint
 class PostFragment:Fragment(R.layout.fragment_post) {
 
     private val mainViewModel: MainViewModel by viewModels()
+    private lateinit var  builder: AlertDialog
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         val binding = FragmentPostBinding.bind(view)
+        builder = setProgressDialog(requireContext(),"Loading...")
         val commentsAdapter = CommentsAdapter()
         val bundle = this.arguments
         var post: Post? = null
@@ -47,23 +58,41 @@ class PostFragment:Fragment(R.layout.fragment_post) {
                 post!!.favorite = 0
                 mainViewModel.updatePost(post)
             }
-
+            addFavorite.visibility = View.GONE
+            removeFavorite.visibility = View.GONE
         }
         mainViewModel.getComments(post?.id!!)
         mainViewModel.comments.observe(viewLifecycleOwner, {
             when (it.status) {
                 Status.SUCCESS -> {
-
+                   builder.dismiss()
                     it.data.let { res ->
+                        if(res!=null){
+                            binding.addFavorite.visibility = View.VISIBLE
+                            binding.removeFavorite.visibility = View.VISIBLE
+                        }else{
+                            binding.addFavorite.visibility = View.GONE
+                            binding.removeFavorite.visibility = View.GONE
+                        }
                         commentsAdapter.submitList(res!!)
                         mainViewModel.insertComments(res)
                     }
                 }
                 Status.LOADING -> {
-
+                    binding.addFavorite.visibility = View.GONE
+                    binding.removeFavorite.visibility = View.GONE
+                   builder.show()
                 }
                 Status.ERROR -> {
+                    builder.dismiss()
                     mainViewModel.getCommentsFromDb(post?.id).observe(viewLifecycleOwner,{
+                        if(it.isNotEmpty()){
+                            binding.addFavorite.visibility = View.VISIBLE
+                            binding.removeFavorite.visibility = View.VISIBLE
+                        }else{
+                            binding.addFavorite.visibility = View.GONE
+                            binding.removeFavorite.visibility = View.GONE
+                        }
                         commentsAdapter.submitList(it)
                     })
                     Snackbar.make(view, it.message!!, Snackbar.LENGTH_SHORT).show()
@@ -75,4 +104,48 @@ class PostFragment:Fragment(R.layout.fragment_post) {
 
     }
 
+    private fun setProgressDialog(context: Context, message:String):AlertDialog {
+        val llPadding = 30
+        val ll = LinearLayout(context)
+        ll.orientation = LinearLayout.HORIZONTAL
+        ll.setPadding(llPadding, llPadding, llPadding, llPadding)
+        ll.gravity = Gravity.CENTER
+        var llParam = LinearLayout.LayoutParams(
+                LinearLayout.LayoutParams.WRAP_CONTENT,
+                LinearLayout.LayoutParams.WRAP_CONTENT)
+        llParam.gravity = Gravity.CENTER
+        ll.layoutParams = llParam
+
+        val progressBar = ProgressBar(context)
+        progressBar.isIndeterminate = true
+        progressBar.setPadding(0, 0, llPadding, 0)
+        progressBar.layoutParams = llParam
+
+        llParam = LinearLayout.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT,
+                ViewGroup.LayoutParams.WRAP_CONTENT)
+        llParam.gravity = Gravity.CENTER
+        val tvText = TextView(context)
+        tvText.text = message
+        tvText.setTextColor(Color.parseColor("#000000"))
+        tvText.textSize = 20.toFloat()
+        tvText.layoutParams = llParam
+
+        ll.addView(progressBar)
+        ll.addView(tvText)
+
+        val builder = AlertDialog.Builder(context)
+        builder.setCancelable(true)
+        builder.setView(ll)
+
+        val dialog = builder.create()
+        val window = dialog.window
+        if (window != null) {
+            val layoutParams = WindowManager.LayoutParams()
+            layoutParams.copyFrom(dialog.window?.attributes)
+            layoutParams.width = LinearLayout.LayoutParams.WRAP_CONTENT
+            layoutParams.height = LinearLayout.LayoutParams.WRAP_CONTENT
+            dialog.window?.attributes = layoutParams
+        }
+        return dialog
+    }
     }
--- app/src/main/java/com/vinayak/justcleanassessment/ui/PostsFragment.kt
+++ app/src/main/java/com/vinayak/justcleanassessment/ui/PostsFragment.kt
@@ -1,7 +1,17 @@
 package com.vinayak.justcleanassessment.ui
 
+import android.app.AlertDialog
+import android.content.Context
+import android.graphics.Color
+import android.net.ConnectivityManager
 import android.os.Bundle
+import android.view.Gravity
 import android.view.View
+import android.view.ViewGroup
+import android.view.WindowManager
+import android.widget.LinearLayout
+import android.widget.ProgressBar
+import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
@@ -15,16 +25,16 @@ import com.vinayak.justcleanassessment.utils.Status
 import com.vinayak.justcleanassessment.viewmodel.MainViewModel
 import dagger.hilt.android.AndroidEntryPoint
 
+
 @AndroidEntryPoint
 class PostsFragment:Fragment(R.layout.fragment_posts), PostsAdapter.OnItemClickListener {
     private val mainViewModel: MainViewModel by viewModels()
-
-
+    private lateinit var  builder: AlertDialog
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         val binding = FragmentPostsBinding.bind(view)
         val postsAdapter = PostsAdapter(this)
-
+        builder = setProgressDialog(requireContext(), "Loading...")
         binding.apply {
             recyclerViewPosts.apply {
                 adapter = postsAdapter
@@ -36,16 +46,17 @@ class PostsFragment:Fragment(R.layout.fragment_posts), PostsAdapter.OnItemClickL
         mainViewModel.posts.observe(viewLifecycleOwner, {
             when (it.status) {
                 Status.SUCCESS -> {
-
+                    builder.dismiss()
                     it.data.let { res ->
                         postsAdapter.submitList(res!!)
                         mainViewModel.insertPosts(res)
                     }
                 }
                 Status.LOADING -> {
-
+                    builder.show()
                 }
                 Status.ERROR -> {
+                    builder.dismiss()
                     mainViewModel.getOfflinePosts().observe(viewLifecycleOwner, {
                         postsAdapter.submitList(it)
                     })
@@ -54,14 +65,60 @@ class PostsFragment:Fragment(R.layout.fragment_posts), PostsAdapter.OnItemClickL
             }
         })
 
+
+
+
     }
 
     override fun onItemClick(post: Post) {
         var bundle = Bundle()
-        bundle.putParcelable("post",post)
-        findNavController().navigate(R.id.action_postsFragment_to_postFragment,bundle)
+        bundle.putParcelable("post", post)
+        findNavController().navigate(R.id.action_postsFragment_to_postFragment, bundle)
     }
 
+    private fun setProgressDialog(context: Context, message: String):AlertDialog {
+        val llPadding = 30
+        val ll = LinearLayout(context)
+        ll.orientation = LinearLayout.HORIZONTAL
+        ll.setPadding(llPadding, llPadding, llPadding, llPadding)
+        ll.gravity = Gravity.CENTER
+        var llParam = LinearLayout.LayoutParams(
+                LinearLayout.LayoutParams.WRAP_CONTENT,
+                LinearLayout.LayoutParams.WRAP_CONTENT)
+        llParam.gravity = Gravity.CENTER
+        ll.layoutParams = llParam
 
+        val progressBar = ProgressBar(context)
+        progressBar.isIndeterminate = true
+        progressBar.setPadding(0, 0, llPadding, 0)
+        progressBar.layoutParams = llParam
+
+        llParam = LinearLayout.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT,
+                ViewGroup.LayoutParams.WRAP_CONTENT)
+        llParam.gravity = Gravity.CENTER
+        val tvText = TextView(context)
+        tvText.text = message
+        tvText.setTextColor(Color.parseColor("#000000"))
+        tvText.textSize = 20.toFloat()
+        tvText.layoutParams = llParam
+
+        ll.addView(progressBar)
+        ll.addView(tvText)
+
+        val builder = AlertDialog.Builder(context)
+        builder.setCancelable(true)
+        builder.setView(ll)
+
+        val dialog = builder.create()
+        val window = dialog.window
+        if (window != null) {
+            val layoutParams = WindowManager.LayoutParams()
+            layoutParams.copyFrom(dialog.window?.attributes)
+            layoutParams.width = LinearLayout.LayoutParams.WRAP_CONTENT
+            layoutParams.height = LinearLayout.LayoutParams.WRAP_CONTENT
+            dialog.window?.attributes = layoutParams
+        }
+        return dialog
+    }
 
 }
